321e8cc7107a0720a24a819314bb1b0052c5bbcf
Parth442002 <parthkatiyar04@gmail.com>
2021-05-17 11:42:43 +0530

    ProjectTemplate basic done

--- src/templates/ProjectTemplate.js
+++ src/templates/ProjectTemplate.js
@@ -2,11 +2,36 @@ import { graphql, Link } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import React from 'react';
 import Dump from '../components/Dump';
-import { Layout } from '../maincomponents/Layout';
+import  Layout  from '../maincomponents/Layout';
 
 function ProjectTemplate ({ data, pageContext }) {
+  const { frontmatter, body } = data.mdx;
+  const { previous, next } = pageContext;
   return(
-
+    <Layout>
+      <h1>{frontmatter.title}</h1>
+      <p>{frontmatter.date}</p>
+      <h5>{frontmatter.tags}</h5>
+      <MDXRenderer>{body}</MDXRenderer>
+      {previous === false ? null : (
+        <>
+          {previous && (
+            <Link to={previous.fields.slug}>
+              <p>{previous.frontmatter.title}</p>
+            </Link>
+          )}
+        </>
+      )}
+      {next === false ? null : (
+        <>
+          {next && (
+            <Link to={next.fields.slug}>
+              <p>{next.frontmatter.title}</p>
+            </Link>
+          )}
+        </>
+      )}
+    </Layout>
   );
 };
 
